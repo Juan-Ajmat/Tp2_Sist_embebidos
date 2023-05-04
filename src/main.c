3aefee99e76a2629f97a65e39f712c876bcc04cb
Juan-Ajmat <juan.ajmat@gmail.com>
2023-05-04 10:38:14 -0300

    CorreccionSintaxis2

--- src/main.c
+++ src/main.c
@@ -29,7 +29,7 @@ int main (void){
 
 char cadena [128];
 alumno_t yo= CrearAlumno("Ajmat", "Juan Ignacio", 41649548);
-alumno_t other= CrearAlumno("Gonzalez", "Mauro", 45555555)
+alumno_t other= CrearAlumno("Gonzalez", "Mauro", 45555555);
 
 
 
@@ -38,7 +38,7 @@ if (serializar(yo, cadena, sizeof(cadena)) >=0){
 }else{
 	printf("Error al serializar\n");
 }
-if (serializar(otro, cadena, sizeof(cadena)) >=0){
+if (serializar(other, cadena, sizeof(cadena)) >=0){
 	printf("%s\n", cadena);
 }else{
 	printf("Error al serializar\n");
